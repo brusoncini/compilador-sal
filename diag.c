a2f15344cebfc4e120181d83e82e822d733633a1
Bruna Soncini <soncinibruna@gmail.com>
2026-05-16 18:59:29 -0300

    feat: centralizar diagnosticos

--- diag.c
+++ diag.c
@@ -1,2 +1,23 @@
-// Centraliza mensagens de erro e rastreamento, com formato padronizando das saídas. Atua como API de alto nível para lex e parser.
-// Interface: diag_error, diag_info
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "diag.h"
+#include "log.h"
+
+void diag_info(const char *mensagem)
+{
+    printf("%s\n", mensagem);
+    log_trace_line(mensagem);
+}
+
+void diag_error(const char *mensagem)
+{
+    printf("Erro: %s\n", mensagem);
+    exit(1);
+}
+
+void diag_error_linha(int linha, const char *mensagem)
+{
+    printf("Erro na linha %d: %s\n", linha, mensagem);
+    exit(1);
+}
---
+++ diag.h
@@ -0,0 +1,8 @@
+#ifndef DIAG_H
+#define DIAG_H
+
+void diag_info(const char *mensagem);
+void diag_error(const char *mensagem);
+void diag_error_linha(int linha, const char *mensagem);
+
+#endif
